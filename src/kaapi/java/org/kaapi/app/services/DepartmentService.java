b46af386e5b08985dc8f7abb87c83aee9d4a107d
Rotha <khoeurn.rotha@gmail.com>
2015-12-27 18:10:42 +0700

    DepartmentService

--- src/kaapi/java/org/kaapi/app/services/DepartmentService.java
+++ src/kaapi/java/org/kaapi/app/services/DepartmentService.java
@@ -1,9 +1,11 @@
 package org.kaapi.app.services;
 import java.util.List;
+
 import org.kaapi.app.entities.Department;
+import org.kaapi.app.entities.Pagination;
 public interface DepartmentService {
 	public boolean createDepartment(Department department);
 	public boolean updateDepartment(Department department);
 	public boolean deleteDepartment(int id);
-	public List<Department> listDepartment(int offset, int limit);
+	public List<Department> listDepartment(Pagination pagination, String keyword);
 }
