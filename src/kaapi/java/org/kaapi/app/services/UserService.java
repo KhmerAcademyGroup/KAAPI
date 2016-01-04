cba99cc237624bf46692909be845b3e776641822
tola <tolapheng99@gmail.com>
2016-01-04 14:03:08 +0700

    UserTypeService

---
+++ src/kaapi/java/org/kaapi/app/services/UserService.java
@@ -0,0 +1,26 @@
+package org.kaapi.app.services;
+
+import java.util.List;
+
+import org.kaapi.app.entities.Pagination;
+import org.kaapi.app.entities.User;
+
+public interface UserService {
+
+	public User mobileLogin(String email , String password);
+	public User webLogin(String email);
+	public List<User> listUser(Pagination pagination);
+	public int countUser();
+	public boolean getUSer(String id);
+	public boolean validateEmail(String email);
+	public boolean insertUser(User user);
+	public boolean updateUser(User user);
+	public boolean deleteUser(String id);
+	public boolean insertCoverPhoto();
+	public boolean updateCoverPhoto(String coverId);
+	
+	public String getPasswordByEmail(String email);
+	public boolean resetPassword(String password);
+	public boolean validateOldPassword(String password);
+	
+}
---
+++ src/kaapi/java/org/kaapi/app/services/UserTypeService.java
@@ -0,0 +1,18 @@
+package org.kaapi.app.services;
+
+import java.util.List;
+
+import org.kaapi.app.entities.UserType;
+
+public interface UserTypeService {
+
+	public List<UserType> listUserType();
+	public int countUserType();
+	public List<UserType> searchUserType(String name);
+	public int countSearchUserType(String name);
+	public UserType getUserType(String userTypeId);
+	public boolean insertUserType(UserType userType);
+	public boolean updateUserType(UserType userType);
+	public boolean deleteUserType(String userTypeId);
+	
+}
