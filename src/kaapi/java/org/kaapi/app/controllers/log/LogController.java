fe083d43742bbc71a57859f8d6c44906009269c4
lie111 <lie111@live.com>
2016-01-05 15:52:20 +0700

    LogController

--- src/kaapi/java/org/kaapi/app/controllers/log/LogController.java
+++ src/kaapi/java/org/kaapi/app/controllers/log/LogController.java
@@ -9,6 +9,7 @@ import org.kaapi.app.services.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,8 @@ public class LogController {
 		@Autowired
 		LogService service;
 		
-		@RequestMapping(value="/log_listcatebyuser", method= RequestMethod.GET, headers= "Accept=application/json")
-		public ResponseEntity<Map<String, Object>> getCateByUser(@RequestParam("userid") int userid){
+		@RequestMapping(value="/log_listcatebyuser/l/{userid}", method= RequestMethod.GET, headers= "Accept=application/json")
+		public ResponseEntity<Map<String, Object>> getCateByUser(@PathVariable("userid") int userid){
 						
 			ArrayList<Log> logs = service.listCategoryInUser(userid);
 			Map<String, Object> map = new HashMap<String, Object>();
@@ -40,7 +41,7 @@ public class LogController {
 			return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);			
 		}
 		
-		@RequestMapping(value="/log_listcategory", method = RequestMethod.GET, headers="Accept=application/json")
+		@RequestMapping(value="/listcategory", method = RequestMethod.GET, headers="Accept=application/json")
 		public ResponseEntity<Map<String,Object>> getCategory(){
 			
 			ArrayList<Log> logs = service.listCategory();
@@ -56,8 +57,8 @@ public class LogController {
 			return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
 		}
 		
-		@RequestMapping(value="/log_listdepartment",method = RequestMethod.GET,headers="Accept=application/json")
-		public ResponseEntity<Map<String,Object>> getDepartment(@RequestParam("univid")int universityid){
+		@RequestMapping(value="/listdepartment/{univid}",method = RequestMethod.GET,headers="Accept=application/json")
+		public ResponseEntity<Map<String,Object>> getDepartment(@PathVariable("univid")int universityid){
 			
 			ArrayList<Log> logs = service.listDeparmentByUniversity(universityid);
 			Map<String,Object> map = new HashMap<String, Object>();
@@ -72,7 +73,7 @@ public class LogController {
 			return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
 		}
 		
-		@RequestMapping(value="/log_listuniversity", method = RequestMethod.GET,headers = "Accept=application/json")
+		@RequestMapping(value="/listuniversity", method = RequestMethod.GET,headers = "Accept=application/json")
 		public ResponseEntity<Map<String,Object>> getUniversity(){
 			
 			ArrayList<Log> logs = service.listUniversity();
@@ -89,8 +90,8 @@ public class LogController {
 			return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
 		}		
 
-		@RequestMapping(value="/log_listuserbydept", method=RequestMethod.GET, headers = "Accept=application/json")
-		public ResponseEntity<Map<String,Object>> getUserByDept(@RequestParam("deptid")int departmentid,@RequestParam("univid")int universityid){
+		@RequestMapping(value="/listuserbydept/d/{deptid}/u/{univid}", method=RequestMethod.GET, headers = "Accept=application/json")
+		public ResponseEntity<Map<String,Object>> getUserByDept(@PathVariable("deptid")int departmentid,@PathVariable("univid")int universityid){
 			
 			ArrayList<Log> logs = service.listUserInDepartmentAndUniversity(departmentid, universityid);
 			Map<String,Object> map = new HashMap<String, Object>();
@@ -106,8 +107,8 @@ public class LogController {
 			return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
 		}
 
-		@RequestMapping(value="/log_listuserbycate", method=RequestMethod.GET, headers = "Accept=application/json")
-		public ResponseEntity<Map<String,Object>> getUserByCate(@RequestParam("cateid")int categoryid){
+		@RequestMapping(value="/listuserbycate/{cateid}", method=RequestMethod.GET, headers = "Accept=application/json")
+		public ResponseEntity<Map<String,Object>> getUserByCate(@PathVariable("cateid")int categoryid){
 			
 			ArrayList<Log> logs = service.listUserInCategory(categoryid);
 			Map<String,Object> map = new HashMap<String, Object>();
