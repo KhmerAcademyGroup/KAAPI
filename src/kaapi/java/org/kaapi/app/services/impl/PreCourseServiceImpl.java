b10f3ef1201322f8c311034c5488586bea695170
lie111 <lie111@live.com>
2016-01-05 13:54:56 +0700

    PreCourseServiceImpl

---
+++ src/kaapi/java/org/kaapi/app/services/impl/PreCourseServiceImpl.java
@@ -0,0 +1,58 @@
+package org.kaapi.app.services.impl;
+
+import java.util.ArrayList;
+
+import org.kaapi.app.entities.PreCourse;
+import org.kaapi.app.services.PreCourseService;
+
+public class PreCourseServiceImpl implements PreCourseService {
+
+	@Override
+	public boolean addPreCourse(PreCourse preCourse) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePreCourse(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean editPreCourse(PreCourse preCourse) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updatePreCourse(PreCourse preCourse) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<PreCourse> getAllPreCourses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkPrecourseStudent(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PreCourse getPreCourse(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PreCourse getPreCourseStudent(int uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
