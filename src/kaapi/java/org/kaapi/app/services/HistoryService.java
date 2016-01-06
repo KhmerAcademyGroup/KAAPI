0ce3d80d7b247f72fddf8b68d5f11e8cfe379e41
chhoin <heistall9@gmail.com>
2016-01-06 11:59:59 +0700

    history change to add encode

--- src/kaapi/java/org/kaapi/app/services/HistoryService.java
+++ src/kaapi/java/org/kaapi/app/services/HistoryService.java
@@ -4,10 +4,10 @@ import org.kaapi.app.entities.History;
 import org.kaapi.app.entities.Pagination;
 
 public interface HistoryService {
-	public History list(String search ,int uid , Pagination pagin);
+	public History list(String search ,String uid , Pagination pagin);
 	public boolean insert(History dto);
-	public boolean delete(int historyid);
-	public boolean deleteAll(int userid);
-	public int count(String search , int userid);
+	public boolean delete(String historyid);
+	public boolean deleteAll(String userid);
+	public int count(String search , String userid);
 	
 }
