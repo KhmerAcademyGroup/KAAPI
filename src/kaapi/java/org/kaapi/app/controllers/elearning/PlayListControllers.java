b26748825b2d51f777e83b2f3ac37061527771f1
chhoin <heistall9@gmail.com>
2016-01-15 16:51:22 +0700

    ssfsf

--- src/kaapi/java/org/kaapi/app/controllers/elearning/PlayListControllers.java
+++ src/kaapi/java/org/kaapi/app/controllers/elearning/PlayListControllers.java
@@ -27,6 +27,7 @@ public class PlayListControllers {
 	@Autowired
 	PlayListServics playlistservice;
 	
+	
 	@RequestMapping(value="/test/{pid}", method= RequestMethod.GET, headers= "Accept=application/json")
 	public ResponseEntity<Map<String, Object>> test(@PathVariable("pid") int playlisid){
 		Map<String, Object> map= new HashMap<String, Object>();
