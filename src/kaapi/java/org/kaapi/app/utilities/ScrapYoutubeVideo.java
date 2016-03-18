75eff54f514908bfaca87bdbbb8c82e61ddab027
tola <tolapheng99@gmail.com>
2016-03-18 16:53:08 +0700

    TTTTTTT

--- src/kaapi/java/org/kaapi/app/utilities/ScrapYoutubeVideo.java
+++ src/kaapi/java/org/kaapi/app/utilities/ScrapYoutubeVideo.java
@@ -9,16 +9,22 @@ import org.jsoup.select.Elements;
 public class ScrapYoutubeVideo {
 
 	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("https://www.youtube.com/playlist?list=PLWznN7ZS7n8XiYJ53d4xqiIfATFPgoAeb").get();
-		Elements elementTitles = doc.select(".pl-video-title a.pl-video-title-link");
-		StringBuilder data = new StringBuilder();
-		for(Element elementTitle : elementTitles){
-			System.out.println(elementTitle.text());
-			System.out.println(elementTitle.attr("href"));
-			System.out.println(elementTitle.attr("href").substring(9, elementTitle.attr("href").indexOf("&")));
-			data.append(elementTitle.text()+"\t"+ elementTitle.attr("href").substring(9, elementTitle.attr("href").indexOf("&"))+"\r\n");
+		try{
+			
+			Document doc = Jsoup.connect("https://www.youtube.com/playlist?list=PLWznN7ZS7n8U-tlROS8R5sYnXOSZZUrIx").get();
+			Elements elementTitles = doc.select(".pl-video-title a.pl-video-title-link");
+			StringBuilder data = new StringBuilder();
+			for(Element elementTitle : elementTitles){
+				System.out.println(elementTitle.text());
+				System.out.println(elementTitle.attr("href"));
+				System.out.println(elementTitle.attr("href").substring(9, elementTitle.attr("href").indexOf("&")));
+				data.append(elementTitle.text()+"\t"+ elementTitle.attr("href").substring(9, elementTitle.attr("href").indexOf("&"))+"\r\n");
+			}
+			System.out.println(data.toString());
+			System.out.println(data.length());
+		}catch(Exception exception){
+			exception.printStackTrace();	
 		}
-		System.out.println(data.toString());
 		
 	}
 }
